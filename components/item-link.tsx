d2c2dbc2d7472f24ce3d553bcd0b67c19ee55390
bjarkt <688414+bjarkt@users.noreply.github.com>
2022-10-18 18:45:58 +0200

    open in new tab

--- components/item-link.tsx
+++ components/item-link.tsx
@@ -3,7 +3,12 @@ import { Badge } from "./badge";
 
 export const ItemLink: React.FC<{ item: RssItem }> = ({ item }) => {
   return (
-    <a href={item.link} className="hover:underline">
+    <a
+      href={item.link}
+      target="_blank"
+      rel="noreferrer"
+      className="hover:underline"
+    >
       <Badge text={item.siteName} /> {item.title}
     </a>
   );
