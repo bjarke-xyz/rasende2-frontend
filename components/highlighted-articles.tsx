68365c5bd076fc3f28edc11915fed67a21b54719
bjarkt <688414+bjarkt@users.noreply.github.com>
2024-08-10 21:42:06 +0200

    fix dark css

--- components/highlighted-articles.tsx
+++ components/highlighted-articles.tsx
@@ -35,7 +35,7 @@ const ArticleCard: React.FC<{ article: FakeNewsItem }> = ({ article }) => {
     const contentPreview = truncateText(article.content, 100);
 
     return (
-        <div className="min-w-[8rem]  shadow-md rounded-lg">
+        <div className="min-w-[8rem] shadow-md rounded-lg">
             <img
                 className="w-full h-48 object-cover"
                 src={article.imageUrl}
@@ -44,24 +44,24 @@ const ArticleCard: React.FC<{ article: FakeNewsItem }> = ({ article }) => {
             />
             <div className="p-4">
                 <div className="flex items-center justify-between mb-2">
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">
+                    <span className="bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">
                         {article.siteName}
                     </span>
-                    <span className="text-gray-500 text-xs">
+                    <span className="text-gray-500 dark:text-gray-200 text-xs">
                         {new Date(article.published).toLocaleDateString()}
                     </span>
                 </div>
-                <h2 className="text-lg font-semibold text-gray-800 hover:underline">
+                <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-50 hover:underline">
                     <a href={url} target="_blank" rel="noopener noreferrer">
                         {article.title}
                     </a>
                 </h2>
-                <p className="text-gray-600 mt-2">{contentPreview}</p>
+                <p className="text-gray-600 dark:text-gray-200 mt-2">{contentPreview}</p>
                 <a
                     href={url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-500 hover:text-blue-700 mt-4 inline-block"
+                    className="text-blue-500 dark:text-blue-200 dark:hover:text-blue-300 hover:text-blue-700 mt-4 inline-block"
                 >
                     Læs mere
                 </a>
